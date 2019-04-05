88487fb40706a1895555a9858f851278e0facde9
Thiébaud Modoux <tmodoux@pryv.com>
2019-04-05 15:25:07 +0200

    Fix hosts entry.

--- build/mail/sendmail.sh
+++ build/mail/sendmail.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -e
-
-line=$(head -n 1 /etc/hosts)
-line2=$(echo $line | awk '{print $2}')
  
-echo "$line $line2.localdomain" >> /etc/hosts
+echo "127.0.0.1 localhost localhost.localdomain $HOSTNAME" >> /etc/hosts
 yes 'Y' | sendmailconfig
