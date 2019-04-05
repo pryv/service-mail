a57789d92089e572b06e133980d1ba289a36b937
Thiébaud Modoux <tmodoux@pryv.com>
2019-04-05 14:57:12 +0200

    Try to fix commands.

--- build/mail/sendmail.sh
+++ build/mail/sendmail.sh
@@ -4,5 +4,5 @@ set -e
 line=$(head -n 1 /etc/hosts)
 line2=$(echo $line | awk '{print $2}')
  
-run echo "$line $line2.localdomain" >> /etc/hosts
-run yes 'Y' | sendmailconfig
+echo "$line $line2.localdomain" >> /etc/hosts
+yes 'Y' | sendmailconfig
