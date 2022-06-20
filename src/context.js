dde6cd2524608d3d7397056d56dae77b34163bbe
Simon Goumaz <simon@attentif.ch>
2022-06-20 10:36:08 +0200

    Add licensing info with `source-licenser`

--- src/context.js
+++ src/context.js
@@ -1,3 +1,9 @@
+/**
+ * @license
+ * Copyright (C) 2018–2022 Pryv S.A. https://pryv.com - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
 const EmailTemplates = require('email-templates');
 const TemplateRepository = require('./mail/template_repository.js').TemplateRepository;
 const Sender = require('./mail/sender.js').Sender;
--- test/unit/settings.test.js
+++ test/unit/settings.test.js
@@ -1,3 +1,9 @@
+/**
+ * @license
+ * Copyright (C) 2018–2022 Pryv S.A. https://pryv.com - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
 /* global describe, it, beforeEach */
 
 const chai = require('chai');
