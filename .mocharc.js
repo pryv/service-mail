307edc42f31605a64409d3e7c06a452d3f32ceb9
Perki <perki@pryv.com>
2025-02-16 15:26:17 +0100

    including Mocharc

--- .mocharc.js
+++ .mocharc.js
@@ -1,3 +1,9 @@
+/**
+ * @license
+ * Copyright (C) Pryv S.A. https://pryv.com
+ * This file is part of Pryv.io and released under BSD-Clause-3 License
+ * Refer to LICENSE file
+ */
 module.exports = {
   exit: true,
   slow: 75,
